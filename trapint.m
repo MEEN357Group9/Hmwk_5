667c636c1dba6c3b38ca6ca37b1fbfcd9a4754ac
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2017-11-01 15:15:59 -0500

    task 1

---
+++ trapint.m
@@ -0,0 +1,34 @@
+function [ I ] = trapint(x,fx)
+% Takes data sample locations and function samplings at those locations as
+% inputs and returns an approximation of the integral over the sample range
+% based on the trapezoidal rule
+%
+% Inputs:
+% x              is the data sample locations
+% fx             is the function samplings at provided location
+%
+% Outputs: 
+% I              is the approximation of the integral over the sample range
+
+% input error checking
+if length(x) ~= length(fx) 
+    error('x and fx need to be the same length')
+elseif ismatrix(x) || ismatrix(fx)
+    error('Check x or fx. They must be arrays, not matrices.')
+elseif ~isnumeric(x) || ~isnumeric(fx)
+    error('Check x or fx. The elements of the array must be numeric.')
+else
+    % good job; continue on
+end
+
+
+% actual function runnning
+I = 0; % start off with 0 as the integral
+for j = 1: (length(x)-1)
+    I2 = (fx(j) + fx(j+1))/2*(x(j+1) - x(j));
+    I = I2 + I;
+end
+
+
+end
+
