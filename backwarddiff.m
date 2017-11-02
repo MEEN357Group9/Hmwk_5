9b96fb4d90c215c84bdfdd462ab2f767a0bbd13a
znavarro13 <zarah.navarro@gmail.com>
2017-11-01 19:51:49 -0500

    gdf

--- backwarddiff.m
+++ backwarddiff.m
@@ -27,10 +27,13 @@ end
 
 
 for i = chk1:-1:1
-    I = (3*fx(i) - 4*fx(i-1) + fx(i-2))/(2*x(i-1) - x(i));
     if fx(i) <= 0
        fx = 0;
     end
+    if fx(i-1) <= 0
+       fx = 0;
+    end
+    I = (3*fx(i) - 4*fx(i-1) + fx(i-2))/(2*x(i-1) - x(i));
 end
 
 
