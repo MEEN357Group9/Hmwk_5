79df2928c401c19e5323efe35efb33fc58b3c8d4
kelseybanasik <32917713+kelseybanasik@users.noreply.github.com>
2017-11-02 22:41:51 -0500

    Commented Task 6

--- a5task6.m
+++ a5task6.m
@@ -3,18 +3,15 @@
 % Assignment 5, Task 6
 % Group 9
 %
- % dT/dt = -k*(T - T_amb)
- % T = temp of the body
- % T_amb = 23 C
- % k = proportionality constant
  
- t = [0,5,10,15,20,25,30,35,40,45,50];
- T = [100,80,50,40,35,30,27,25,24,23.5,23.5];
+ t = [0,5,10,15,20,25,30,35,40,45,50]; % Time [s] when temperature was recorded
+ T = [100,80,50,40,35,30,27,25,24,23.5,23.5]; % Temperature of the body [deg C]
  
  T_amb = 23; % Degrees C
  equ = T(3:9) - T_amb; % starts off at 3 to get the same vector array as backwarddiff produces 
- I = backwarddiff(t,T);
+ I = backwarddiff(t,T); % calculates dT/dt using backwards differentiation
 
+ % Plots dT/dt versus T - T_amb
  figure
  plot(equ,I)
  
