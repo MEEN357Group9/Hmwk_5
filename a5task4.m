f7692913edb227329cea23cb67c29e2d3a12aebe
znavarro13 <zarah.navarro@gmail.com>
2017-11-02 20:18:13 -0500

    For Harland

--- a5task4.m
+++ a5task4.m
@@ -1,8 +1,8 @@
 % a5task4.m
 clc;
 clearvars;
-ha=[0,1,2,3,4,5,6,7,8,9,10;...
-    0.00,0.31,.99,1.77,2.40,2.73,2.55,2.07,1.32,.585,0.00];
+ha=[0,1,2,3,4,5,6,7,8,9,10;... %%%%%% this row should be 0:3:27, idk why
+    0.00,0.31,.99,1.77,2.40,2.73,2.55,2.07,1.32,.585]; %%%%%%% also apparently we're supposed to take off the last zero in this second row according to the TA
 a=2*ha(2,:);%whole area in in^2
 h=3;
 x=3*ha(1,:);
